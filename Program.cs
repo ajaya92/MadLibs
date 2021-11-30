717b59caa3a9a799562b960710137b5b4ed278f9
Ajaya Shrestha <ajaya92@gmail.com>
2021-11-30 06:59:00 -0600

    Variables added

--- Program.cs
+++ Program.cs
@@ -23,6 +23,23 @@ namespace MadLibs
             // Define user input and variables:
             Console.Write("Enter a name: ");
             string name = Console.ReadLine();
+            Console.Write("Enter a word: ");
+            string word = Console.ReadLine();
+
+            Console.Write("Enter a first adjective: ");
+            string adj1 = Console.ReadLine();
+
+            Console.Write("Enter a verb: ");
+            string verb = Console.ReadLine();
+
+            Console.Write("Enter a first noun: ");
+            string noun1 = Console.ReadLine();
+
+            Console.Write("Enter a second noun: ");
+            string noun2 = Console.ReadLine();
+
+            Console.Write("Enter one of the following(- An animal \n - A food \n - A fruit \n - A superhero \n - A country \n - A dessert \n - A year): ");
+            string weird = Console.ReadLine();
 
             // The template for the story:
 
